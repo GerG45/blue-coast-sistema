630ae7b03e221d156837ac192752b35c4730cd0e
GerG45 <german.lozano45@gmail.com>
2026-08-11 21:03:53 -0300

    Limpia icono de linea de tiempo

--- modulos/checkin/app.js
+++ modulos/checkin/app.js
@@ -93,7 +93,7 @@ const THEME_PREF_KEY = "bluecoast-theme-v1";
 const TITLE_FONT_URL = new URL("./fonts/libre-baskerville-bold.ttf", window.location.href).href;
 const ROOM_ICON_SINGLE_URL = new URL("./assets/cama-single.svg", window.location.href).href;
 const ROOM_ICON_DOUBLE_URL = new URL("./assets/cama-doble.svg", window.location.href).href;
-const TIMELINE_ICON_URL = new URL("../../assets/module-icons/linea-de-tiempo.svg", window.location.href).href;
+const TIMELINE_ICON_URL = new URL("../../assets/module-icons/linea-de-tiempo.svg?v=20260811-2", window.location.href).href;
 const SUMMARY_ICON_URLS = Object.freeze({
   openReservations: new URL("./assets/resumen/reservas-abiertas.svg", window.location.href).href,
   confirmedReservations: new URL("./assets/resumen/reservas-confirmadas.svg", window.location.href).href,
